deff65dfd01da839754f5f18a26c8337e3168994
BZajc <bbb.zaj@gmail.com>
2024-03-15 02:18:02 +0100

    Changed design and structure of mobile navigation

--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RxCross2, RxChevronRight, RxChevronLeft } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import {
   FaPeopleGroup,
   FaNewspaper,
@@ -9,22 +9,23 @@ import {
   FaQuestion,
   FaSquarePhone,
   FaSquareEnvelope,
+  FaHouse,
   FaFacebook,
   FaXTwitter,
   FaYoutube,
   FaInstagram,
   FaTiktok,
 } from "react-icons/fa6";
-import { useDispatch} from "react-redux";
-import { setShowNav} from "../store/slices/navMobileSlice";
+import { useDispatch } from "react-redux";
+import { setShowNav } from "../store/slices/navMobileSlice";
 
 function NavMobile() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleCloseClick = () => {
-    dispatch(setShowNav(false))
+    dispatch(setShowNav(false));
     console.log("Close clicked!");
-  }
+  };
 
   return (
     <nav>
@@ -32,45 +33,49 @@ function NavMobile() {
         <button className="nav-mobile__close-btn" onClick={handleCloseClick}>
           <RxCross2 />
         </button>
-        <div className="nav-mobile__info-box">
-          <h3 className="nav-mobile__info-topic">What's New?</h3>
-          <p className="nav-mobile__info-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            dolorum laboriosam beatae reiciendis velit suscipit!
-          </p>
-          <button className="nav-mobile__info-btn">
-            <RxChevronLeft className="nav-mobile__info-btn-chevron" />
-            Learn More{" "}
-            <RxChevronRight className="nav-mobile__info-btn-chevron" />
-          </button>
-        </div>
-        <div className="nav-mobile__icons">
-          <button className="nav-mobile__icon-box">
-            <FaNewspaper className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">What's New?</p>
-          </button>
-          <button className="nav-mobile__icon-box">
-            <FaPeopleGroup className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">Classes</p>
-          </button>
-          <button className="nav-mobile__icon-box">
-            <FaUser className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">Membership</p>
-          </button>
-          <button className="nav-mobile__icon-box">
-            <FaStar className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">Personal Trainer</p>
-          </button>
-          <button className="nav-mobile__icon-box">
-            <FaMoneyCheckDollar className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">Promo for Juniors</p>
-          </button>
-          <button className="nav-mobile__icon-box">
-            <FaQuestion className="nav-mobile__icon" />
-            <p className="nav-mobile__icon-description">FAQ</p>
-          </button>
-        </div>
+        <ul className="nav-mobile__list">
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaNewspaper className="nav-mobile__icon" />
+              <p className="nav-mobile__description">What's New?</p>
+            </button>
+          </li>
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaPeopleGroup className="nav-mobile__icon" />
+              <p className="nav-mobile__description">Classes</p>
+            </button>
+          </li>
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaUser className="nav-mobile__icon" />
+              <p className="nav-mobile__description">Membership</p>
+            </button>
+          </li>
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaStar className="nav-mobile__icon" />
+              <p className="nav-mobile__description">Personal Trainer</p>
+            </button>
+          </li>
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaMoneyCheckDollar className="nav-mobile__icon" />
+              <p className="nav-mobile__description">Promo for Juniors</p>
+            </button>
+          </li>
+          <li className="nav-mobile__item">
+            <button className="nav-mobile__btn">
+              <FaQuestion className="nav-mobile__icon" />
+              <p className="nav-mobile__description">FAQ</p>
+            </button>
+          </li>
+        </ul>
         <div className="nav-mobile__contact-box">
+          <p className="nav-mobile__contact-info">
+            We are here to help you! Feel free to contact us with any questions
+            or support needs.
+          </p>
           <div className="nav-mobile__contact-data-box">
             <a href="tel:+23 555 213 749" className="nav-mobile__contact-data">
               <FaSquarePhone /> +23 555 213 749
@@ -81,34 +86,12 @@ function NavMobile() {
             >
               <FaSquareEnvelope /> somegym@support.com
             </a>
-            <p className="nav-mobile__contact-data">Wonderland 23b</p>
+            <p className="nav-mobile__contact-data">
+              <FaHouse />
+              Wonderland 23b
+            </p>
           </div>
-          <p className="nav-mobile__contact-info">
-            We are here to help you! Feel free to contact us with any questions
-            or support needs.
-          </p>
         </div>
-        <div className="nav-mobile__socials-box">
-          <button className="nav-mobile__social-icon">
-            <FaFacebook />
-          </button>
-          <button className="nav-mobile__social-icon">
-            <FaXTwitter />
-          </button>
-          <button className="nav-mobile__social-icon">
-            <FaYoutube />
-          </button>
-          <button className="nav-mobile__social-icon">
-            <FaInstagram />
-          </button>
-          <button className="nav-mobile__social-icon">
-            <FaTiktok />
-          </button>
-        </div>
-
-        <p className="nav-mobile__visual-info">
-          Links for socials are only for a visual purposes
-        </p>
       </div>
     </nav>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,27 +1,38 @@
-import React from "react";
-import Header from "../components/Header";
-import NavMobile from "../components/NavMobile";
-import ImagesSlider from "../components/ImagesSlider";
-import { useSelector } from "react-redux";
-import { selectShowNav } from "../store/slices/navMobileSlice";
-import Offer from "../components/Offer";
-// import Partnership from "../components/Partnership";
-import PersonalTrainer from "../components/PersonalTrainer";
-import ProgressBar from "../components/ProgressBar";
 
-function MainPage() {
-  const showNav = useSelector(selectShowNav);
-  return (
-    <div className="main-page">
-      {showNav && <NavMobile />}
-      <ProgressBar />
-      <Header />
-      <ImagesSlider />
-      <Offer />
-      {/* <Partnership /> */}
-      <PersonalTrainer />
-    </div>
-  );
-}
+  import React, { useEffect } from "react";
+  import Header from "../components/Header";
+  import NavMobile from "../components/NavMobile";
+  import ImagesSlider from "../components/ImagesSlider";
+  import { useSelector } from "react-redux";
+  import { selectShowNav } from "../store/slices/navMobileSlice";
+  import Offer from "../components/Offer";
+  // import Partnership from "../components/Partnership";
+  import PersonalTrainer from "../components/PersonalTrainer";
+  import ProgressBar from "../components/ProgressBar";
+
+  function MainPage() {
+    const showNav = useSelector(selectShowNav);
+
+    // Remove scroll when nav is open
+    useEffect(() => {
+      if (showNav) {
+        document.body.style.overflowY = "hidden";
+      } else {
+        document.body.style.overflowY = "auto";
+      }
+    }, [showNav]);
+
+    return (
+      <div className="main-page">
+        {showNav && <NavMobile />}
+        <ProgressBar />
+        <Header />
+        <ImagesSlider />
+        <Offer />
+        {/* <Partnership /> */}
+        <PersonalTrainer />
+      </div>
+    );
+  }
 
 export default MainPage;
